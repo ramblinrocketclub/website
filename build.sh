ffa00609e9b595585c0e99b90381b1dde3d0d6df
Adit Sachde <23707194+aditsachde@users.noreply.github.com>
2022-07-01 17:03:44 -0400

    disable production deployment

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z $CF_PAGES ]; then
     zola build
-elif [ "$CF_PAGES_BRANCH" == "main" ]; then
+elif [ "$CF_PAGES_BRANCH" == "devel" ]; then
     echo "Building production deployment with url rocketry.pages.dev"
     zola build --base-url rocketry.pages.dev
 else
