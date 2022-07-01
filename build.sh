4f213c91b5611fe0ac132106e34ee6b2ab159d53
Adit Sachde <23707194+aditsachde@users.noreply.github.com>
2022-07-01 17:04:59 -0400

    use https prefix for base url

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ -z $CF_PAGES ]; then
     zola build
-elif [ "$CF_PAGES_BRANCH" == "devel" ]; then
-    echo "Building production deployment with url rocketry.pages.dev"
-    zola build --base-url rocketry.pages.dev
+elif [ "$CF_PAGES_BRANCH" == "main" ]; then
+    echo "Building production deployment with url https://rocketry.pages.dev"
+    zola build --base-url https://rocketry.pages.dev
 else
     echo "Building preview deployment with url $CF_PAGES_URL"
     zola build --base-url $CF_PAGES_URL
