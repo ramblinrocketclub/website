8ace68b73e11d21506254754e6d4e79a16e3a520
Adit Sachde <23707194+aditsachde@users.noreply.github.com>
2022-07-01 17:01:33 -0400

    add build script debug logs

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 if [ -z $CF_PAGES ]; then
     zola build
 elif [ "$CF_PAGES_BRANCH" == "main" ]; then
+    echo "Building production deployment with url rocketry.pages.dev"
     zola build --base-url rocketry.pages.dev
 else
+    echo "Building preview deployment with url $CF_PAGES_URL"
     zola build --base-url $CF_PAGES_URL
 fi
